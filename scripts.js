25e92204492a86868935a49d6357c99794c7a8d8
Jade Bowen <jade.bowen.career@gmail.com>
2024-10-22 08:41:39 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,10 +14,3 @@ window.addEventListener('load', function() {
         mainContent.classList.remove('hidden');  // Display the main content
     });
 });
-
-// Dark Mode Toggle
-const toggleButton = document.getElementById('toggleTheme');
-toggleButton.addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-    toggleButton.textContent = document.body.classList.contains('dark-mode') ? 'Switch to Light Mode' : 'Switch to Dark Mode';
-});
